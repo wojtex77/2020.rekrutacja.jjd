e066a5e32955208303054cea95a16ad95eae37da
wojtex77 <37085911+wojtex77@users.noreply.github.com>
2022-06-20 21:28:57 +0200

    small refactoring

--- src/pl/wojciechsiwek/CompositeBlock.java
+++ src/pl/wojciechsiwek/CompositeBlock.java
@@ -5,7 +5,7 @@ import java.util.List;
 public interface CompositeBlock extends Block{
 
 //    Nie za bardzo rozumiem, dlaczego w tym miejscu jest nagłówek metody getBlocks();
-//    Ten interfejs rozszerzając Block winien raczej dodawać nowe cechy dla elementu block a nie zwracać jakąś listę,
+//    Ten interfejs rozszerzając Block winien raczej dodawać nowe cechy-pola dla elementu block a nie zwracać jakąś listę,
 //    która nie wiadomo do czego się odnosi. W moim rozwiązaniu ten interfejs wydaje się zbędny.
 
     List getBlocks();
--- src/pl/wojciechsiwek/Wall.java
+++ src/pl/wojciechsiwek/Wall.java
@@ -7,16 +7,17 @@ import java.util.Optional;
 public class Wall implements Structure {
     private List blocks;
 
+//    zwraca obiekt typu Block lub null aby można było użyć strumienia do filtrowania z użyciem metod
+//    zawartych w interfejsie Block
     private Block getBlockOrNull(Object block) {
+
         return block instanceof Block ? ((Block) block) : null;
     }
 
     @Override
     public Optional findBlockByColor(String color) {
 
-
         return blocks.stream().filter(block -> Objects.requireNonNull(getBlockOrNull(block)).getColor().equals(color)).findAny();
-
     }
 
     @Override
